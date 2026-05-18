776de3b3900e7e59cfef600631f6fbc8eab2f42e
Rulezt <progetti2web@gmail.com>
2026-05-18 14:27:19 +0200

    v3.8.30 - Mobile: header mini-chart scrollabile lateralmente

--- crypto_scanner_pro/rootfs/app/app.py
+++ crypto_scanner_pro/rootfs/app/app.py
@@ -275,7 +275,7 @@ def health():
     
     return jsonify({
         'status': 'ok',
-        'version': '3.8.29',
+        'version': '3.8.30',
         'telegram_configured': telegram_configured,
         'telegram_token_set': bool(config['telegram']['token']),
         'telegram_chat_id_set': bool(config['telegram']['chat_id']),
